d1d4b7435259e33d4a3d8b7a49600b7768333162
Sophio <sofolaz@gmail.com>
2023-03-27 19:50:30 +0400

    up123

---
+++ pixe-test/script.js
@@ -0,0 +1,12 @@
+function myFunction(){ 
+    document.getElementById('demo').innerHTML = 'Hello Sophie';
+}
+function myFunction2(){
+    document.getElementById('demo').innerHTML = 'Hello Latsuzbaia'
+}
+
+const x = "";
+   console.log(parseFloat(x));
+
+let brands = ["prada", "balenciaga", "dior"]
+console.log(brands)
